ef064fd16fae2a94ab6739b4a93fc389868fcd2a
Alejandro Lahoz <alejandrolahozdefez@gmail.com>
2021-06-10 23:05:02 +0200

    Testing connection load

--- main/server.py
+++ main/server.py
@@ -1,24 +1,29 @@
 import socket
-from connection_load import connect_with_server
+from connection_load import Connection
+
 
 class Server():
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
+  def __init__(self, host, port):
+    self.host = host
+    self.port = port
 
-    def start_server(self):
-        print('Host:', self.host, 'Port:', self.port)
-        # socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # socket_server.bind((self.host, self.port))
-        # socket_server.listen(5)
-        connect_with_server()
+  def start_server(self):
+    print('Host:', self.host, 'Port:', self.port)
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_server.bind((self.host, self.port))
+    socket_server.listen(5)
 
-    def connections(self)    :
-        pass
+  def test(self):
+    conn = Connection(self.host, self.port)
+    conn.connect_with_server()
 
-    def commands(self):
-        # command = conn.recv(2048).decode()
-        pass
-    
+  def connections(self):
+    pass
+
+  def commands(self):
+    # command = conn.recv(2048).decode()
+    pass
+
 server = Server('192.168.1.129', 5555)
-server.start_server()
+# server.start_server()
+server.test()
